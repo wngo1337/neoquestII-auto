3e257b4ded5387d4c2483f943886e892efa58b54
wngo1337 <wngo1337@gmail.com>
2025-09-24 01:31:16 -0400

    Remove dupe handler for logger

--- src/logging_config.py
+++ src/logging_config.py
@@ -1,16 +1,6 @@
 import logging
-import sys
-
-# Don't know why, but logging stopped working suddenly before we added this
-for handler in logging.root.handlers[:]:
-    logging.root.removeHandler(handler)
 
 logging.basicConfig(
     level=logging.DEBUG,  # or DEBUG for detailed logs during development
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
 )
-
-console = logging.StreamHandler(sys.stdout)
-console.setLevel(logging.INFO)
-# add the handler to the root logger
-logging.getLogger('').addHandler(console)
